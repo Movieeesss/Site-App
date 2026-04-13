85c47cc699d221bc042e6976b103abdc19fe1c90
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-13 16:35:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SiteReport from './SiteReport' // Import the new component
+import SiteReport from './SiteApp' // Import the new component
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
